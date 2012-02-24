998a93f9a05302dfea08117dd9d3f79ffe877d86
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2012-02-24 17:12:54 +0800

    Disposable interface example added

---
+++ ch5/src/main/java/com/apress/prospring3/ch5/lifecycle/DestructiveBeanWithInterface.java
@@ -0,0 +1,53 @@
+package com.apress.prospring3.ch5.lifecycle;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DestructiveBeanWithInterface implements InitializingBean, DisposableBean {
+
+    private InputStream is = null;
+    public String filePath = null;
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing Bean");
+        if (filePath == null) {
+            throw new IllegalArgumentException(
+                    "You must specify the filePath property of " + DestructiveBeanWithInterface.class);
+        }
+        is = new FileInputStream(filePath);
+
+    }
+
+    public void destroy() {
+        System.out.println("Destroying Bean");
+        if (is != null) {
+            try {
+                is.close();
+                is = null;
+            } catch (IOException ex) {
+                System.err.println("WARN: An IOException occured"
+                        + " trying to close the InputStream");
+            }
+        }
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public static void main(String[] args) throws Exception {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:lifecycle/disposeInterface.xml");
+        ctx.refresh(); // Refresh the ApplicationContext after XML config file loaded
+        
+        DestructiveBeanWithInterface bean = (DestructiveBeanWithInterface) ctx.getBean("destructiveBean");
+        
+        System.out.println("Calling destroy()");
+        ctx.destroy();
+        System.out.println("Called destroy()");
+    }
+}
